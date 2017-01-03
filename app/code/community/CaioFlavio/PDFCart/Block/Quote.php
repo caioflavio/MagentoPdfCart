73a0c594a2d05215ab781eb43d5427a3f20996a1
Caio Flavio <caioflavio2@hotmail.com.br>
2017-01-03 10:14:09 -0200

    added: funções pro layout

--- app/code/community/CaioFlavio/PDFCart/Block/Quote.php
+++ app/code/community/CaioFlavio/PDFCart/Block/Quote.php
@@ -1,5 +1,17 @@
  <?php
 	class CaioFlavio_PDFCart_Block_Quote extends Mage_Core_Block_Template{
+		public function getPageTitle($storeid){
+			return Mage::getStoreConfig('pdfcart/general/page_title', $storeid);
+		}
+
+		public function getLogoPath($storeid){
+			return $this->getSkinUrl('images/'.Mage::getStoreConfig('pdfcart/general/logo_name', $storeid));
+		}
+
+		public function getDate(){
+			return Mage::getModel('core/date')->date('d/m/Y H:i:s');
+		}
+		
 		public function getQuoteData(){
 			$cart  		= Mage::getModel('checkout/cart')->getQuote();
 			$cartItems  = array();
