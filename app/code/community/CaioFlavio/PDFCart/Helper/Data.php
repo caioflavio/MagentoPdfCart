1cf81418961dc94c0e72372c1672fc602898fccc
Caio Flavio <caioflavio2@hotmail.com.br>
2017-06-20 09:45:34 -0300

    added: helpers para links de ações

--- app/code/community/CaioFlavio/PDFCart/Helper/Data.php
+++ app/code/community/CaioFlavio/PDFCart/Helper/Data.php
@@ -1,4 +1,10 @@
 <?php 
 	class CaioFlavio_PDFCart_Helper_Data extends Mage_Core_Helper_Abstract{
-	
+		public function getQuoteLink($label='Salvar Cotação', $class = ''){
+			return "<a href='" . Mage::getBaseUrl() ."pdfcart/index/index' class='". $class ."'>".$label."</a>";
+		}
+
+		public function getEmailLink($label='Enviar Cotação', $class = ''){
+			return "<a href='" . Mage::getBaseUrl() ."pdfcart/index/sendEmail' class='". $class ."'>".$label."</a>";
+		}
 	}
